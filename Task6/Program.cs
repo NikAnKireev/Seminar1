7b2afdf174496490bc7a823bec5a0e88a1d65478
Nikita Kireev <kireevinvest@yandex.ru>
2022-04-21 13:35:21 +0300

    new folder Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+using System;
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int N=int.Parse(Console.ReadLine());
+
+if (N%2==0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
